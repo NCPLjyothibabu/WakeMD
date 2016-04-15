0e85d4f54f26479fdefb9b91f9935cc67d84f705
ncplinc <ncplinc@ramesh-babus-mac-mini.local>
2016-04-15 13:41:43 +0530

    Initial Commit

---
+++ WakeMD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WakeMD
+//
+//  Created by NCPL on 4/15/16.
+//  Copyright © 2016 NCPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WakeMD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WakeMD
+//
+//  Created by NCPL on 4/15/16.
+//  Copyright © 2016 NCPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
